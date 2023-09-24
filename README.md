b115c20a33e876f7ab5d88f29d5e95485e5dd3cd
rohitsalla <sallarohit10@gmail.com>
2023-09-24 08:44:09 +0530

    first

---
+++ README.md
@@ -0,0 +1,5 @@
+# OneNeuron Python Package
+
+## Reference -
+
+* [Official python docs for PYPI] (https://packaging.python.org/en/latest/tutorials/packaging-projects/)
---
+++ setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+PROJECT_NAME = "oneNeuron_pypi"
+USER_NAME = "rohitsalla"
+
+setuptools.setup(
+    name=f"{PROJECT_NAME}-{USER_NAME}",
+    version="0.0.2",
+    author=USER_NAME,
+    author_email="sallarohit1011@gmail.com",
+    description="its an implimentation of Perceptron",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{USER_NAME}/{PROJECT_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{USER_NAME}/{PROJECT_NAME}/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.7",
+    install_requires=[
+        "numpy==1.21.4",
+        "pandas==1.3.4",
+        "joblib==1.1.0"
+    ]
+)
